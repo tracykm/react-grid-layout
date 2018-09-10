eb1b4c7125a73abcf6107169c41a8d80fad92f07
Adam Alcott <aalcott14@gmail.com>
2018-09-10 10:27:36 -0500

    srsly another typeo

--- build/ReactGridLayout.js
+++ build/ReactGridLayout.js
@@ -119,7 +119,7 @@ var ReactGridLayout = (function(_React$Component) {
     this.onLayoutMaybeChanged(this.state.layout, this.props.layout);
   };
 
-  ReactGridLayout.prototype.UNSAFE_scomponentWillReceiveProps = function UNSAFE_scomponentWillReceiveProps(
+  ReactGridLayout.prototype.UNSAFE_componentWillReceiveProps = function UNSAFE_componentWillReceiveProps(
     nextProps
   ) {
     var newLayoutBase = void 0;
